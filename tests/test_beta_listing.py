3396a1d235b41ee0eae745bbe79d8c81c3d1beae
Ajay Kotian <ajay.kotian@csusb.edu>
2015-08-06 09:01:54 -0700

    Tests for Author profile

--- tests/test_beta_listing.py
+++ tests/test_beta_listing.py
@@ -1,14 +1,16 @@
 __author__ = 'jwsm'
 
-from FanfictionAPI import beta
+import sys
+sys.path.append('../FanfictionAPI/')
+from FanfictionAPI import beta 
 import unittest
-
+from FanfictionAPI.author import Author
 
 class TestBetas(unittest.TestCase):
 
     def setUp(self):
         self.beta_list = beta.BetaListing('https://www.fanfiction.net/betareaders/game/Final-Fantasy-X/',
-                                           open('betas_src/beta_page_base.html', 'r').read())
+                                           open('tests/betas_src/beta_page_base.html', 'r').read())
 
     def test_language_filter(self):
         self.beta_list.set_language_filter('English')
@@ -26,6 +28,15 @@ class TestBetas(unittest.TestCase):
         new_page = self.beta_list.get_page(5)
         self.assertIn('&ppage=5', new_page.build_filtered_url())
 
+    def test_author_profiles(self):
+        self.beta_list = beta.BetaListing('https://www.fanfiction.net/betareaders/game/Final-Fantasy-X/')
+        self.assertIsInstance(next(self.beta_list.author_profiles()), Author, 'Correct type returned')
+        self.beta_list = beta.BetaListing('https://www.fanfiction.net/betareaders/game/Tales-of-the-Abyss')
+        self.assertIsInstance(next(self.beta_list.author_profiles()), Author, 'Correct type returned')
+        self.beta_list = beta.BetaListing('https://www.fanfiction.net/betareaders/game/Flipnote-Studio')
+        self.assertIsInstance(next(self.beta_list.author_profiles()), Author, 'Correct type returned')
+        self.beta_list = beta.BetaListing('https://www.fanfiction.net/betareaders/game')
+        self.assertEqual(self.beta_list.author_profiles(), None)
 
 if __name__ == '__main__':
     unittest.main()
